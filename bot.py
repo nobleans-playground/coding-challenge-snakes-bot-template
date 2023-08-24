357a05cf53c2f34876d63241610ee4c368f3c9bb
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-08-24 13:46:20 +0200

    Add documentation & simplify code

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from ...bot import Bot
 from ...constants import Move, MOVE_VALUE_TO_DIRECTION
+from ...snake import Snake
 
 
 def is_on_grid(pos, grid_size):
@@ -28,32 +29,33 @@ class SimpleEater(Bot):
     def name(self):
         return 'Simple Eater'
 
-    def determine_next_move(self, snakes, candies) -> Move:
+    def determine_next_move(self, snakes: List[Snake], candies: List[np.array]) -> Move:
+        # First find your own snake
         snake = next(s for s in snakes if s.id == self.id)
+        other_snake = next(s for s in snakes if s.id != self.id)
 
+        moves = self._determine_possible_moves(snake, other_snake)
+        return self.choose_towards_candy(moves, snake, candies)
+
+    def _determine_possible_moves(self, snake, other_snake) -> List[Move]:
+        """
+        Return a list with all moves that we want to do. Later we'll choose one from this list randomly. This method
+        will be used during unit-testing
+        """
         # highest priority, a move that is on the grid
         on_grid = [move for move in MOVE_VALUE_TO_DIRECTION
                    if is_on_grid(snake[0] + MOVE_VALUE_TO_DIRECTION[move], self.grid_size)]
         if not on_grid:
-            return self.choose_towards_candy(list(Move), snake, candies)
+            return list(Move)
 
         # then avoid collisions with other snakes
         collision_free = [move for move in on_grid
                           if is_on_grid(snake[0] + MOVE_VALUE_TO_DIRECTION[move], self.grid_size)
-                          and not collides(snake[0] + MOVE_VALUE_TO_DIRECTION[move], snakes)]
-        if not collision_free:
-            return self.choose_towards_candy(on_grid, snake, candies)
-
-        # then avoid the heads of other snakes
-        avoids_snakes = [move
-                         for move in collision_free
-                         for other_snake in snakes
-                         if other_snake is not snake
-                         if np.linalg.norm((snake[0] + MOVE_VALUE_TO_DIRECTION[move]) - other_snake[0], 1) > 1]
-        if not avoids_snakes:
-            return self.choose_towards_candy(collision_free, snake, candies)
-
-        return self.choose_towards_candy(avoids_snakes, snake, candies)
+                          and not collides(snake[0] + MOVE_VALUE_TO_DIRECTION[move], [snake, other_snake])]
+        if collision_free:
+            return collision_free
+        else:
+            return on_grid
 
     def choose_towards_candy(self, moves: List[Move], snake, candies):
         if not candies:
